f712308126f65e74f75065eb9e64643dfbcfea17
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-14 19:01:09 +0330

    Developed the object

--- include/game/object.h
+++ include/game/object.h
@@ -1,16 +1,38 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
-#include "include/core/point.h"
+#include <windows.h>
+#include <iostream>
+#include "Config.h"
+#include "Console.h"
+#include "Point.h"
 
-class Object
+class Object 
 {
-    void setCurrentPoint(Point const &value);
-    Point getCurrentPoint() const;
+public:
+    // Draw the object on the view.
     virtual void draw() = 0;
 
+    // Erase or clear the object from the view.
+    virtual void erase() = 0;
+
+    // Move the object to a new position in the view.
+    virtual void move() = 0;
+
+    // Handles keyboard input and processes key events.
+    virtual void handler_keys() = 0;
+
+    // Handles the current point of the object.
+    inline Point currentPoint() const {
+        return _current;
+    }
+    inline Point setCurrentPoint(Point const &value) {
+        _current = value;
+    }
+
 protected:
-    Point _point;
+    Point _current;
+    std::string _skin;
 };
 
 #endif
