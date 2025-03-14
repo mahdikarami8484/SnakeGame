90d7ac5e28d8f025d5a303e159c62a4f1d9bc592
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-14 19:12:12 +0330

    Removed some whitespaces

--- include/core/View.h
+++ include/core/View.h
@@ -1,20 +1,19 @@
 #ifndef VIEW_H
 #define VIEW_H
 
-#include "core/Config.h"
-#include "core/Console.h"
+#include "include/core/config.h"
+#include "include/core/console.h"
 
 class View 
 {
-    public:
+public:
+    virtual ~View() {}
 
-        virtual ~View() {}
+    // Call during runtime
+    virtual void start() = 0;
 
-        // Call during runtime
-        virtual void start() = 0;
-
-        // Call on every frame
-        virtual void update() = 0;
+    // Call on every frame
+    virtual void update() = 0;
 };
 
 #endif
