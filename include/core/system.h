14a1a52dbbb3bd6eb138e2c7fe5d2ec1e072f9c0
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-16 04:58:47 +0330

    Added a function to put the program into sleep

--- include/core/system.h
+++ include/core/system.h
@@ -4,4 +4,5 @@
 
 namespace System {
     int64_t GenerateNumber(int64_t minimum, int64_t maximum);
+    void Delay(uint64_t duration);
 }
--- src/core/system.cpp
+++ src/core/system.cpp
@@ -1,4 +1,5 @@
 #include "core/system.h"
+#include <windows.h>
 #include <random>
 
 int64_t System::GenerateNumber(int64_t minimum, int64_t maximum)
@@ -8,4 +9,9 @@ int64_t System::GenerateNumber(int64_t minimum, int64_t maximum)
     mt19937_64 engine(device());
     uniform_int_distribution<int> dist(minimum, maximum);
     return dist(engine);
+}
+
+void System::Delay(uint64_t duration)
+{
+    Sleep(duration);
 }
