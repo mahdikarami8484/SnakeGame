ef5a6fa777ee0152dc57e2dd237510f546a941bc
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-16 13:07:10 +0330

    Provided a function to obtain the skin of an object

--- include/objects/object.h
+++ include/objects/object.h
@@ -6,20 +6,16 @@
 class Object
 {
 public:
-    // Draw the object on the view.
     virtual void draw() = 0;
-
-    // Erase or clear the object from the view.
+    virtual void move() = 0;
     virtual void erase() = 0;
 
-    // Move the object to a new position in the view.
-    virtual void move() = 0;
-
-    // Handles the current point of the object.
     Point CurrentPoint() const;
     void SetCurrentPoint(Point const &value);
 
 protected:
+    virtual char GetSkin() const = 0;
+
+protected:
     Point _current;
-    std::string _skin;
 };
