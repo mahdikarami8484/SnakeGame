b1e8b93ced08843f667644dc1662be9d7ccb5241
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-17 00:31:40 +0330

    Implemented erase and draw functionality for food

--- src/objects/food.cpp
+++ src/objects/food.cpp
@@ -1,5 +1,13 @@
 #include "objects/food.h"
 
+void Food::Draw()
+{
+    Graphics::Draw(this->GetSkin(), Graphics::Property(GetForeground(), GetBackground()), this->_current);
+}
+void Food::Erase()
+{
+    Graphics::Draw(" ", Graphics::Property(GetForeground(), GetBackground()), this->_current);
+}
 std::string Food::GetSkin() const
 {
     return "*";
