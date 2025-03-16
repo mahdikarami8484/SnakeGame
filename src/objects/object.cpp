ca016727e68c66f7ce2615289d7807bcec7635ac
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-17 00:34:52 +0330

    The object will get updated automatically as the current point changes

--- src/objects/object.cpp
+++ src/objects/object.cpp
@@ -4,7 +4,9 @@ Point Object::CurrentPoint() const {
     return _current;
 }
 void Object::SetCurrentPoint(Point const &value) {
+    this->Erase();
     _current = value;
+    this->Draw();
 }
 
 void Object::Move() {}
