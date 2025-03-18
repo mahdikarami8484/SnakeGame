066c0fdf4e6fb3fb6ab0badf0ecd12036c8fc27c
mahdikarami8484 <mahdikarami8484.ir@gmail.com>
2025-03-18 10:52:14 +0330

    add ViewList class

--- include/views/viewController.h
+++ include/views/viewController.h
@@ -5,16 +5,26 @@
 
 class viewController {
 
+    protected:
+        class ViewList {
+            public:
+                void addView(std::string const &name, std::shared_ptr<View> view);
+                void GetViewByName(std::string const &name);
+            
+            protected:
+                std::vector<std::shared_ptr<View>> views;
+                std::unordered_map<std::string, std::shared_ptr<View>> name_map;
+        };
+
     public:
-
-        void load(int const view) const;
-        
-
+        void load(ViewList view) const;
         void GetCurrentView() const;
+        void loadNextView() const;
+
+    public:
+        void GetViewList() const;
 
     protected:
+        ViewList viewList;
 
-    protected:
-        int _currentView;
-        std::map<int, View> _views;
 };
